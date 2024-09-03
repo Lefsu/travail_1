61c960fb5f4210cfe2be0a8e77f5512dfa93be20
lefsu <sullivan.lefevre@protonmail.com>
2024-09-03 23:04:26 +0200

    sql

---
+++ database.sql
@@ -0,0 +1,36 @@
+-- DATABASE FOR MANAGING SECURITY AUDITS
+--Employees (id(PK), last name, first name, position, department_id(FK))
+--Systems (id(PK), name, type, department_id(FK), manager_id(FK))
+--Security Audits (id(PK), date, status, employee_id(FK), system_id(FK))
+--Vulnerabilities detected (id(PK), audit_id(FK), system_id(FK), severity, status)
+--Department (id(PK), manager_id(FK), type, status, location)
+-- CREATION OF THE DB
+-- EMPLOYEES TABLE
+CREATE TABLE Employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    position VARCHAR(255),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES Department(id)
+);
+-- SYSTEMS TABLE
+CREATE TABLE Systems (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255),
+    department_id INT,
+    manager_id INT,
+    FOREIGN KEY (department_id) REFERENCES Department(id),
+    FOREIGN KEY (manager_id) REFERENCES Employees(id)
+);
+--SECURITY AUDITS TABLE
+CREATE TABLE SecurityAudits (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE NOT NULL,
+    status VARCHAR(255),
+    employee_id INT,
+    system_id INT,
+    FOREIGN KEY (employee_id) REFERENCES Employees(id),
+    FOREIGN KEY (system_id) REFERENCES Systems(id)
+);
